db983d65adf7a7ea98cd9725b27c65b015f4bcb3
Sultan-html <shalava1321@gmail.com>
2025-01-20 22:27:09 +0600

    Базовая реализация поиска подходящей пары

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 n, k = map(int, input().split())
-numbers = list(map(int, input().split()))v
+numbers = list(map(int, input().split()))
+
+best_sum = float('-inf')
+pair = None
+
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        current_sum = numbers[i] + numbers[j]
+        if abs(k - current_sum) < abs(k - best_sum):
+            best_sum = current_sum
+            pair = (numbers[i], numbers[j])
